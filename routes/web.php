b2f4a3f5c2e1966d4f01bb57d2e5fbe7705f428e
chauthanhnhan <chauthanhnhan@gmail.com>
2022-08-19 09:53:13 +0700

    0.1

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard.index');
+
+Route::get('/auth/twitter', [App\Http\Controllers\Social\TwitterController::class, 'redirectToTwitter'])->name('auth.twitter');
+Route::get('/auth/twitter/callback', [App\Http\Controllers\Social\TwitterController::class, 'redirectToTwitter'])->name('auth.twitter_callback');
+Route::get('/twitter/calculating', [App\Http\Controllers\Social\TwitterController::class, 'calculating'])->name('twitter_calculating');
